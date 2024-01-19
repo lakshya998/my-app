d43ecda76f566314fde0dec697159eb2a5bed034
lakshya998 <111784043+lakshya998@users.noreply.github.com>
2024-01-19 17:21:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Lets Get Started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
