e5c76a6dc1b30008e2554f306d1216d17e6c022b
lakshya998 <111784043+lakshya998@users.noreply.github.com>
2024-01-20 14:46:12 +0530

    push2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    
+
+  ];
+
   return (
     <div>
       <h2>Lets Get Started</h2>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,17 @@
+import './ExpenseItem.css';  
+
+
 function ExpenseItem(){
+    const expenseDate = new Date(2024,0,20);
+    const expenseTitle = 'Insurance';
+    const expenseAmount = 300;
+
    return (
-    <div>
-        <div>Expense Items</div>
-        <div>
-            <ul>
-            <li>Food Rs 10</li>
-            <li>Petrol Rs 100</li>
-            <li>Movies Rs 200</li>
-            </ul>
-            
-        </div>
+    <div className='expense-item' >
+        <div>{expenseDate.toISOString()}</div>
+        <div className='expense-item__description'>
+            <h2>{expenseTitle}</h2></div>
+        <div className='expense-item__price'>${expenseAmount}</div>
     </div>
    );
 }
